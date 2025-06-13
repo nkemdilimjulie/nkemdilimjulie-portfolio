f2770c77ab2d0f55e9b65d6e5c862bcbb081a7e9
nkemdilim <Julie>
2025-06-13 10:35:19 +0200

    Deploy updated site

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,4 +11,6 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
   introVideoUrl = 'https://www.youtube.com/shorts/FW3G5SxtR8A'
+  pitch = 'https://www.loom.com/share/8d44714290d84f589dcf301919877f1c?sid=e1259f8f-477c-4478-8977-07e37fbf5394'
+  tech = 'https://www.loom.com/share/76be12bb286d4f3696dee2e2f25809b4?sid=fd8ecbf8-8df0-4df4-b518-ad76ee47fbf7'
 }
